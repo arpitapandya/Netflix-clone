2a05b8c877647dae29f9779ab490674acf9452ab
Arpita <arpya74@gmail.com>
2021-03-05 15:32:37 -0500

    added dropdown feature

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-import { Background, ButtonLink, Container, Logo } from './styles/header';
+import { Background, ButtonLink, Container, Dropdown, Link, Logo, Group, Profile, Picture, Feature, Text, FeatureCallOut } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background { ...restProps}>{children}</Background> : children;
 }
 
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut { ...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature { ...restProps}>{children}</Feature>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown { ...restProps}>{children}</Dropdown>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text { ...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+    return <Link { ...restProps}>{children}</Link>;
+};
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container { ...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group { ...restProps}>{children}</Group>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+    return <Profile { ...restProps}>{children}</Profile>;
 };
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -5,8 +5,17 @@ export const Background = styled.div`
     display: flex;
     flex-direction: column;
     background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover no-repeat
+
+@media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) => 
+    dontShowOnSmallViewPort && `background: none;`}
+}
 `;
 
+export const Group = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 export const Container = styled.div`
     display: flex;
@@ -22,6 +31,41 @@ export const Container = styled.div`
 
     @media (max-width: 1000px) {
         margin: 0 30px;
+    }
+`;
+
+export const Picture = styled.button`
+    background: url(${({ src }) => src});
+    background-size: contain;
+    border: 0;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+`;
+
+export const Dropdown = styled.div`
+    display: none;
+    background-color: black;
+    position: absolute;
+    padding: 10px;
+    width: 100px;
+    top: 32px;
+    right: 10px;
+`;
+
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+    position: relative;
+
+    button: {
+        cursor: pointer;
+    }
+
+    &:hover > ${Dropdown} {
+        display: flex;
+        flex-direction: column;
     }
 `;
 
@@ -54,4 +98,50 @@ export const ButtonLink = styled(ReactRouterLink)`
         background-color: #f40612;
     }
 `;
+
+export const Feature = styled(Container)`
+    padding: 150px 0 500px 0;
+    flex-direction: column;
+    align-items: normal;
+    width: 50%;
+
+    @media (max-width: 1100px) {
+        display: none;
+    }
+`;
+
+export const Text = styled.p`
+    color: white;
+    font-size: 22px;
+    line-height: normal;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
+`;
+
+export const Link = styled.p`
+    color: white;
+    text-decoration: none;
+    margin-right: 30px;
+    font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+    cursor: pointer;
+
+    &:hover {
+        font-weight: bold;
+    }
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
+
+export const FeatureCallOut = styled.h2`
+    color: white;
+    font-size: 50px;
+    line-height: normal;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+    margin: 0;
+    margin-bottom: 20px;
+`;
+
 
