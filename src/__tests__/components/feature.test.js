ba24ca638170ab73b706b24662cbc9c1d872103a
Arpita <arpya74@gmail.com>
2021-04-03 11:25:27 -0400

    tested feature

---
+++ src/__tests__/components/feature.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Feature } from "../../components";
+
+describe("<Feature />", () => {
+    it("renders the <Feature /> with populated data", () => {
+        const { container, queryByText, getByText } = render(
+            <Feature>
+                <Feature.Title>Unlimited films, TV programms and more.</Feature.Title>
+                <Feature.SubTitle>Watch anywhere, Cancel at anytime.</Feature.SubTitle>
+            </Feature>
+        );
+
+        expect(getByText("Unlimited films, TV programms and more.")).toBeTruthy();
+        expect(queryByText("Watch anywhere, Cancel at anytime.")).toBeTruthy();
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it("renders the <Feature /> with just a title", () => {
+        const { container, queryByText, getByText } = render(
+            <Feature>
+                <Feature.Title>Unlimited films, TV programms and more.</Feature.Title>
+            </Feature>
+        );
+
+        expect(getByText("Unlimited films, TV programms and more.")).toBeTruthy();
+        expect(queryByText("Watch anywhere, Cancel at anytime.")).toBeFalsy();
+        expect(container.firstChild).toMatchSnapshot();
+    });
+});
