81f7ac26f0df2c5dba6ae83b5c15dc3a25011658
Arpita <arpya74@gmail.com>
2021-04-05 13:07:11 -0400

    fixed bug & made responsive

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Accordion } from '../components';
-import OptForm from "../components/opt-form";
-import faqsData from '../fixtures/faqs.json';
+import { Accordion, OptForm } from '../components';
+import faqsData from '../fixtures/faqs';
 
 export function FaqsContainer() {
     return (
         <Accordion>
-            <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Frame>
             {faqsData.map((item) => (
                 <Accordion.Item key={item.id}>
                     <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.Body}</Accordion.Body>
+                    <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
-            <Accordion.Item />
+        </Accordion.Frame>
 
             <OptForm>
                 <OptForm.Input placeholder="Email address" />
